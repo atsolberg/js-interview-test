be1723fe9ed574d37aebdb15010c0a4a4d0c98cc
aaron.solberg <aaron.solberg@selectcomfort.com>
2016-06-27 16:39:13 -0500

    fixing instructions

--- README.md
+++ README.md
@@ -6,12 +6,12 @@ A JavaScript problem to solve before interviews.
 ## TO SETUP
 - Clone the repo.
 - Open `index.html` in your browser.
-- Open `js/test.js` and `js/answer.js` in your favorite editor.
+- Open `js/Person.js`, `js\PersonTreeNode.js`, and `js/answer.js` in your favorite editor.
 - Add your code to the `generateTree` function in `answer.js`.
 
 ## SOLVING THE TEST
 - The test uses two JavaScript classes `Person` and `PersonTreeNode`.
 - The `generateTree` function recieves a list of `Person` objects and should return the root `PersonTreeNode` (the ceo).
-- Each `PersonTreeNode`'s `directReports` array should contain a `PersonTreeNode` for each direct report.
-- The `js/test.js` file will use your `generateTree` function in the `js/answer.js` file to see if passes the test.
+- Each `PersonTreeNode`'s `directReports` array should contain a `PersonTreeNode` for each direct report of that person (someone who reports directly for you, i.e. you are their manager).
+- The app will use your `generateTree` function to see if passes the test.
 - Refresh the page or click the button to rerun the test.
--- js/answer.js
+++ js/answer.js
@@ -1,37 +1,24 @@
 
 /**
- * Given an array of Person objects, should produce the root PersonTreeNode (the CEO)
- * and each person's list of direct reports is correct all the way down the tree.
- * @param {Array[<Person>]} employees - An array of Person objects representing
- *                                      all the employees of the company.
+ * Given an array of Person objects, returns the root PersonTreeNode (the CEO).
+ * @param {Person[]} employees - An array of Person objects representing all the employees of the company.
  */
 function generateTree(employees) {
 
   /**
    * INSTRUCTIONS: 
-   * 1. Analyze the Person and PersonTreeNode classes in the test.js file.
-   *    Those two constructor functions are the only things you should use
-   *    from the test.js file.
+   * 1. ONLY edit this function and nothing else!.
    *
-   * 2. DO NOT EDIT the test.js file. The index.html file will use the 
-   *    test.js file to call this function.
+   * 2. Analyze the Person.js and PersonTreeNode.js files.
    *
-   * 3. Parse the 'employees' array and build a single PersonTreeNode
-   *    that will represent the fictional company's organization.
-   *    Refresh the index.html page to retry the test.
-   *  
-   *  Things to note:
-   *  'employees' is an array of objects of type Person.
+   * 3. Parse the `employees` array and create a single PersonTreeNode
+   *    object representing the CEO (the Person with no `manager`).
+   *    All PersonTreeNode object's `directReports` arrays should contain 
+   *    PersonTreeNode's for their direct reports...creating a tree.
    *
-   *  The 'manager' property of the ceo Person will be null.
+   * 4. Refresh or click the 'Retry Test' button to rerun the test.
    *  
-   *  This function should create and return the root PersonTreeNode (the ceo).
-   *  
-   *  Each PersonTreeNode's 'directReports' array should contain a PersonTreeNode 
-   *  for each direct report of that person. A 'direct report' is someone who 
-   *  reports directly for you, i.e. you are their manager.
-   *  
-   *  Create any additional functions in this file as needed.
+   *  Feel free to create any additional functions in this file as needed.
    */
 
   var ceo = null; // Should be a PersonTreeNode object at the end;
